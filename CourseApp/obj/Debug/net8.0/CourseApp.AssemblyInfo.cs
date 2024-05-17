a8c403e01a91a1c80a64a9bdc285cbcdfee0d434
Abdullah Şentürk <asenturk22@yahoo.com>
2024-05-16 20:28:25 -0700

    CourseApp | View/Course/Index.cshtml dosyasi olusturuldu. Receiving Data islemi gerceklestirildi.

--- CourseApp/obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
+++ CourseApp/obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e364bf4e9a4ac524637ef81cb28791374f18acf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d4bbdf1af8006a699131383ebe102e6d9c6b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
