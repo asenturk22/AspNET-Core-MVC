027d7533df396bdf3f94802280f5e9ba29a4513c
Abdullah Şentürk <asenturk22@yahoo.com>
2024-05-19 01:27:36 -0700

    StoreApp | Implement MVC pattern

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+//MVC controller kullanilacak ve Views nesnelerinin de projeye dahil et
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
+app.UseRouting(); 
+
+app.MapControllerRoute(
+    name:"default", 
+    pattern:"{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.Run();
